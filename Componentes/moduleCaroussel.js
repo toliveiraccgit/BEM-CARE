38c4d7be0fec845c2f928fb6f1e82edfd752568a
Diogo-Ol <oliveiradiogo50@gmail.com>
2022-03-04 14:12:08 -0300

    Iniciando e Finalizando página de Planos de Bem.Care

---
+++ Componentes/moduleCaroussel.js
@@ -0,0 +1,7 @@
+import { Carousel } from "./carrousel.js"
+
+const anterior = '[data-anterior]'
+const proximo = '[data-proximo]'
+const listaDepoimentos = '[data-lista-depoimentos]'
+
+new Carousel(anterior, proximo, listaDepoimentos)
